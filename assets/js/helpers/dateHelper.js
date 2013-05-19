9259046726be6e9c5ac0179cc0ff21f5ad74183c
Soulou <leo@unbekandt.eu>
2013-05-19 18:27:21 +0200

    Fix date format

--- assets/js/helpers/dateHelper.js
+++ assets/js/helpers/dateHelper.js
@@ -1,10 +1,10 @@
 toReadableDate = function(date) {
-  var day = date.getDay() < 10 ? "0" + date.getDay() : date.getDay()
-  var month = date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()
-  var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours()
-  var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()
-  var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()
+  var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  var month = date.getMonth() < 9 ? "0" + (date.getMonth()+1) : date.getMonth()+1;
+  var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 
-  return day + "/" + month + "/" + date.getFullYear() 
+  return day + "/" + month + "/" + date.getFullYear()
     + " " + hours + ":" + minutes + ":" + seconds
 };
